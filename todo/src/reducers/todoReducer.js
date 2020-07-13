7b637c610a426552d72ccddd7c2c1bead8501ec2
Daniel Hoesle <dhoesle21@gmail.com>
2020-07-13 13:49:51 -0600

    completed task 1

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,21 @@
+export const initalState = [
+    {
+        task: 'Replace alternator in Camry',
+        completed: false,
+        id: 1528817077286
+    },
+    {
+        task: 'Replace differential fluid in 4runner',
+        completed: false,
+        id: 1528817084358
+    },
+    {
+        task: 'CHILL THE EFF OUT',
+        completed: false,
+        id: 1528817084462
+    }
+]
+
+export const reducer = (state, action) => {
+    return state;
+}
